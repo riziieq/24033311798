6713dbc3502aa66e76a62861a0139db731a6828c
riziieq <riziqpro@gmail.com>
2025-10-24 14:43:19 +0700

    Tugas JavaScript

---
+++ tugas javascript/login.js
@@ -0,0 +1,49 @@
+const token = ~~(Math.random() * 1e8).toString(16);
+const tabungan = [1000000, 2000000, 3000000];
+
+function login(user) {
+  console.log('Login berhasil');
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (user !== "bikyx") reject("User tidak ditemukan");
+      resolve(token);
+    }, 2000);
+  });
+}
+
+function getUser(token) {
+  if (token) {
+    return { key: "C3541" };
+  }
+}
+
+function getTabungan(key) {
+  return new Promise((success, reject) => {
+    if (key) {
+      setTimeout(() => {
+        success(tabungan);
+      }, 2000);
+    } else {
+      reject("User tidak ditemukan");
+    }
+  });
+}
+
+function getSaldo(tabungan) {
+  return new Promise((success) => {
+    if (tabungan) {
+      setTimeout(() => {
+        let total = 0;
+        tabungan.forEach(item => {
+          total += item;
+        });
+        success(total);
+      }, 2000);
+    } else {
+      success(0);
+    }
+  });
+}
+
+// Export fungsi-fungsi agar bisa diimpor
+export { login, getUser, getTabungan, getSaldo };
